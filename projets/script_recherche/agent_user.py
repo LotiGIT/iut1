081f7ef7a9baff60d229932a8793091bf6b5f637
Eliott Janot <eliott.janot@hotmail.com>
2024-08-01 15:42:18 +0200

    avancements

--- projets/script_recherche/agent_user.py
+++ projets/script_recherche/agent_user.py
@@ -6,3 +6,6 @@ url = "https://www.google.com/search?q=clotilde+aberkane"
 
 # Erreur 403 : Forbidden  (le site web refuse de répondre à la requête)
 headers = {
+
+    ### Il faut que je fasse une recherche python d'un film ou serie sur les sites de streaming illegaux qui ne sont aps répertié par google
+    ##juststream.id
--- projets/script_recherche/search.py
+++ projets/script_recherche/search.py
@@ -14,7 +14,7 @@ config = Config()
 config.browser_user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 
 #requete de recherche ( ' " " ' pour rechercher une expression exacte)
-query = "clotilde aberkane"
+query = "the batman streaming vosfr"
 
 # specification du chemin complet du fichier 
 chemin_fichier = "C:\\Users\\eliot\\Documents\\iut1\\projets\\script_recherche\\resultats.txt"
